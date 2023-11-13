1f9d2ff18abefee221300647f26703ac02684195
adrianrtownsend <greenlight8004@gmail.com>
2023-11-13 15:01:16 -0500

    feat: added helper functions

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,1 +1,9 @@
 export const isObjectEmpty = (obj) => Object.keys(obj).length === 0;
+
+export const truncateString = (str: string, limit = 10) => {
+  if (str.length <= limit) {
+    return str;
+  }
+  // Return str truncated with '...' concatenated to the end of str.
+  return str.slice(0, limit) + "...";
+};
